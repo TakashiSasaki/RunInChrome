4bc85c80f2ae47f8c823230a30ffdad55e7f0887
Takashi SASAKI <takashi316@gmail.com>
2018-04-27 06:13:42 +0900

    support userDataDir

--- playground/launcherTest.js
+++ playground/launcherTest.js
@@ -1,14 +1,39 @@
 const chromeLauncher = require("chrome-launcher");
 
+chromePathCandidates = [
+  "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe",
+  "/mnt/c/Program Files/Google/Chrome/Application/chrome.exe",
+];
+
 async function launchChrome(){
-  const chrome = await chromeLauncher.launch({
-    chromeFlags: [
-    ],
-    chromePath: "/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe"
-  });
+  var chrome;
+  for(let i in chromePathCandidates) {
+    console.log(chromePathCandidates[i]);
+    try {
+      chrome = await chromeLauncher.launch({
+        port: 21212,
+        chromeFlags: [
+        ],
+        chromePath: chromePathCandidates[i],
+        userDataDir: false,
+        connectionPollInterval : 2,
+        maxConnectionRetries: 1,
+      });
+      console.log(typeof chrome);
+      if(typeof chrome !== "undefined") return chrome;
+    }catch(e){
+      console.log(e);
+    }//try
+  }//for
 
 }//launchChrome
 
-launchChrome().then(()=>console.log("succeeded."))
-.catch((e)=>console.log(e));
+launchChrome()
+  .then(chrome => {
+    console.log("succeeded.");
+    console.log("chrome.port = " + chrome.port);
+    console.log("chrome.pid = " + chrome.pid);
+  })
+.then(()=>console.log("exiting."))
+.then(()=>process.exit());
 
